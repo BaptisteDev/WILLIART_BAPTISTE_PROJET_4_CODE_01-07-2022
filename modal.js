2adb62306c954e54c1b94367f18fe41cf2e8b3a1
BaptisteDev <61820472+BaptisteDev@users.noreply.github.com>
2022-07-01 18:03:37 +0200

    Todo #2 Début (id et for)

--- modal.js
+++ modal.js
@@ -12,20 +12,21 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
+// Variable pour permettre la fermeture de la modal
+const modalBtnClose = document.querySelectorAll(".close");
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// Event sur la modal pour la fermeture
+modalBtnClose.forEach((btn) => btn.addEventListener("click", closeModal));
 
 // launch modal form
 function launchModal() {
     modalbg.style.display = "block";
 }
 
-// Mise en place de la fermeture de la modal
-
-const modalBtnClose = document.querySelectorAll(".close");
-
-modalBtnClose.forEach((btn) => btn.addEventListener("click", closeModal));
-
+// Fonction qui permet la fermeture de la modal
 function closeModal() {
     modalbg.style.display = "none";
 }
